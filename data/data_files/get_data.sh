dfd258c04aa1a33b952b04305d0748d0a926634b
Thomas Bombrun <thomas.bombrun@gmail.com>
2016-02-01 22:21:55 +0100

    small reorganization

--- data/data_files/get_data.sh
+++ data/data_files/get_data.sh
@@ -1,6 +1,3 @@
-mkdir data_files
-cd data_files
-
 wget http://data.assemblee-nationale.fr/static/openData/repository/LOI/scrutins/Scrutins_XIV.json.zip
 wget http://data.assemblee-nationale.fr/static/openData/repository/AMO/deputes_actifs_mandats_actifs_organes/AMO10_deputes_actifs_mandats_actifs_organes_XIV.json.zip
 
