b083762eff706f4fe328ff8ee48c0175ec780994
Thomas Bombrun <thomas.bombrun@gmail.com>
2016-02-01 22:21:40 +0100

    Creating sqlaccess library to insert values in db

--- data/schema.sql
+++ data/schema.sql
@@ -4,34 +4,32 @@ CREATE TYPE vote_type AS ENUM ('SPO','MOC','SPS','SAT');
  * SPS-scrutin public solennel
  * SAT-scrutin à la tribune
 */
-CREATE TYPE choice_vote AS ENUL('FOR','AGAINST','ABSTAINED','NONVOTING')
+CREATE TYPE choice_vote AS ENUM('FOR','AGAINST','ABSTAINED','NONVOTING');
 
+DROP TABLE votes;
+DROP TABLE deputes;
+DROP TABLE ballots;
 
 CREATE TABLE deputes (
-	id SMALLINT PRIMARY KEY, -- export.acteurs.acteur[i].uuid['#text'].substring(2)
+	id INTEGER PRIMARY KEY, -- export.acteurs.acteur[i].uuid['#text'].substring(2)
 	first_name VARCHAR(20), -- export.acteurs.acteur[i].etatCivil.ident.prenom
 	last_name VARCHAR(20), -- export.acteurs.acteur[i].etatCivil.ident.nom
 	is_male BOOLEAN, -- export.acteurs.acteur[i].etatCivil.ident.civ == "M."
 	birthdate DATE, -- to_date(export.acteurs.acteur[i].etatCivil.infoNaissance.dateNais,'YYYY-MM-DD'))
-	job_name VARCHAR(20), -- export.acteurs.acteur[i].profession.libelleCourant
-	job_category VARCHAR(20), -- export.acteurs.acteur[i].profession.socProcINSEE.catSocPro
-	job_family VARCHAR(20), -- export.acteurs.acteur[i].profession.socProcINSEE.famSocPro
+	job_name VARCHAR(80), -- export.acteurs.acteur[i].profession.libelleCourant
+	job_category VARCHAR(50), -- export.acteurs.acteur[i].profession.socProcINSEE.catSocPro
+	job_family VARCHAR(25) -- export.acteurs.acteur[i].profession.socProcINSEE.famSocPro
 );
 
 CREATE TABLE ballots (
-	id INTEGER PRIMARY KEY, -- scrutins.scrutin[i].uid
+	id INTEGER PRIMARY KEY, -- scrutins.scrutin[i].numero
 	date_vote DATE, -- to_date(scrutins.scrutin[i].dateScrutin : '2012-07-03','YYYY-MM-DD')
 	adopted BOOLEAN, -- scrutins.scrutin[i].sort.code == "adopté"
-	name VARCHAR(500), -- scrutins.scrutin[0].titre
-	number_voter SMALLINT,
-	number_valid_voter SMALLINT,
-	number_for SMALLINT,
-	number_against SMALLINT,
-	number_abstention SMALLINT,
-	number_non_voting SMALLINT
+	name VARCHAR(500) -- scrutins.scrutin[0].titre
 );
+
 CREATE TABLE votes (
-	id_depute SMALLINT REFERENCES deputes,
+	id_depute INTEGER REFERENCES deputes,
 	id_ballot INTEGER REFERENCES ballots,
 	choice choice_vote
 );
