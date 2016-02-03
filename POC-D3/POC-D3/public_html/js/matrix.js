b4c68ecf76c3ff5a7e7e52dbcd145cbc5784898a
Cédric Garnier <cedric.garnier@ensimag.grenoble-inp.fr>
2016-02-03 12:13:07 +0100

    modif color, visu avec logarithme

--- POC-D3/POC-D3/public_html/js/matrix.js
+++ POC-D3/POC-D3/public_html/js/matrix.js
@@ -8,7 +8,7 @@ function init() {
     canvas = document.getElementById("MonCanvas");
     ctx = canvas.getContext("2d");
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    $.getJSON("datdata.json", function(data) {
+    $.getJSON("similarity.json", function(data) {
       console.log("json charger " +(Date.now()-chrono));
       chrono = Date.now();
       drawMatrix(data);
@@ -18,14 +18,14 @@ function init() {
  function drawMatrix(tab) {
     var nb = tab.depute.length;
     for (var i = tab.links.length - 1; i >= 0; i--) {
-      var color = tab.links[i].value/500 *255;
+      var color = Math.floor(37.48*Math.log(tab.links[i].value+1));
 
       var x = tab.links[i].source;
       var y = tab.links[i].target;
       if (x<=y) {console.log("c'est la merde!!!");}
       ctx.fillStyle = "rgb("+color+","+color+","+color+")";
       ctx.fillRect(x, y, x+1, y+1);
-      //ctx.fillRect(y, x, y+1, x+1);
+      ctx.fillRect(y, x, y+1, x+1);
 
     }
     console.log("matrix draw " +(Date.now()-chrono));
